89853a274a4f5b7f78a2791a078734014cde1780
Eugene <xcode@example.com>
2021-10-20 14:39:36 -0700

    passcode fix

--- Passwords/Passcode.swift
+++ Passwords/Passcode.swift
@@ -21,23 +21,40 @@ open class Passcode: NSObject {
     
     public static func  authenticateUser(message: String, completion: @escaping (_ authenticated: Bool, _ error: Error?) -> Void) {
         
-        completion(true, nil)
+//        completion(true, nil)
 
         
-//        let context = LAContext()
-//        
-//        if #available(iOS 9.0, *) {
-//            context.evaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, localizedReason: message) { (success, error) in
-//                DispatchQueue.main.async {
-//                    completion(success, error)
-//                }
-//            }
-//        } else {
-//            DispatchQueue.main.async {
-//                completion(false, nil) // should never be execuded due to isDevicePasscodeSet will be false for older devices
-//            }
-//        }
+        let context = LAContext()
+        
+        var error: NSError?
+        if context.canEvaluatePolicy(.deviceOwnerAuthentication, error: &error) {
+            context.evaluatePolicy(LAPolicy.deviceOwnerAuthentication, localizedReason: message) { (success, error) in
+                DispatchQueue.main.async {
+                    completion(success, error)
+                }
+            }
+        } else {
+            if let err = error {
+                if #available(iOS 11.0, *) {
+                    switch err.code {
+                    case LAError.Code.biometryNotEnrolled.rawValue:
+                        completion(false, "User is not enrolled" as? Error)
+                    case LAError.Code.passcodeNotSet.rawValue:
+                        completion(false, "Pass code not set" as? Error)
+                    case LAError.Code.biometryNotAvailable.rawValue:
+                        completion(false, "Biometrics are not available" as? Error)
+                    default:
+                        completion(false, "Pass code not set" as? Error)
+                    }
+                } else {
+                    // Fallback on earlier versions
+                }
+
+            }
+        }
+        
     }
+    
     
 }
 
